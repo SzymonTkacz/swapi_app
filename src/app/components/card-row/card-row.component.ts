ab49a683ce930d3e55c504108f5077ad4be069e9
Szymon Tkacz <extetszt@universal-robots.com>
2024-11-22 01:07:22 +0100

    added loaders

--- src/app/components/card-row/card-row.component.ts
+++ src/app/components/card-row/card-row.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-card-row',
   standalone: true,
-  imports: [TruncatePipe],
+  imports: [TruncatePipe, MatProgressBarModule],
   templateUrl: './card-row.component.html',
   styleUrl: './card-row.component.scss'
 })
